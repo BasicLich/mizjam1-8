297c0d06c194994181c3e6ab6c7c43af1c2b22b9
Grant <grant@tuxinator.net>
2020-08-19 09:41:37 -0700

    Add basic instructions and menu hovers

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var delay_timer
+
 onready var music = get_node("background_music")
 
 
@@ -14,11 +16,46 @@ func _ready():
 
 func _on_play_button_pressed():
 	var tween_out = get_node("Tween")
-	tween_out.interpolate_property(music, "volume_db", -8, -40, 2.5, 1, Tween.EASE_OUT, 0)
+	tween_out.interpolate_property(music, "volume_db", -8, -30, 8.0, 1, Tween.EASE_OUT, 0)
 	tween_out.start()
 	
-	get_tree().change_scene("res://game_root.tscn")
+	$main_menu.visible = false
+	$instructions.visible = true
+	
+	# show the instructions for 8 seconds
+	delay_timer = Timer.new()
+	delay_timer.wait_time = 8.0
+	delay_timer.autostart = true
+	delay_timer.one_shot = true
+	delay_timer.connect("timeout", self, "start_game")
+	add_child(delay_timer)
 
 
 func _on_exit_button_pressed():
 	get_tree().quit()
+
+
+func _input(event):
+	# any key press starts the game from the instructions screen
+	if $instructions.visible and event is InputEventKey and event.pressed:
+		start_game()
+
+
+func start_game():
+	get_tree().change_scene("res://game_root.tscn")
+
+
+func _on_play_button_mouse_entered():
+	$main_menu/play_button/selected.visible = true
+
+
+func _on_play_button_mouse_exited():
+	$main_menu/play_button/selected.visible = false
+
+
+func _on_exit_button_mouse_entered():
+	$main_menu/exit_button/selected.visible = true
+
+
+func _on_exit_button_mouse_exited():
+	$main_menu/exit_button/selected.visible = false
